f4e1e80db7fded813e8db7f2d11164b1cb89afc3
Moshe Shahar <moshe.shahar@arm.com>
2021-03-24 14:48:22 +0200

    Update build_manylinux_wheels.sh

--- build_manylinux_wheels.sh
+++ build_manylinux_wheels.sh
@@ -19,7 +19,7 @@
 
 set -e -u -x
 
-mkdir dist
+mkdir -p dist
 
 # work in parallel
 docker run --rm -e PLAT=manylinux1_x86_64     -v `pwd`:/io quay.io/pypa/manylinux1_x86_64     /io/build_manylinux_wheels_entry_point.sh &
@@ -27,3 +27,5 @@ docker run --rm -e PLAT=manylinux2010_x86_64  -v `pwd`:/io quay.io/pypa/manylinu
 docker run --rm -e PLAT=manylinux2014_x86_64  -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64  /io/build_manylinux_wheels_entry_point.sh &
 
 wait
+
+sudo chown -R $USERNAME:$USERNAME dist/
